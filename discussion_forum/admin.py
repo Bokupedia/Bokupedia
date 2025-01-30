5cad9304864b0faf74cda4675aea05bb1030c4dd
DogukanTaskiran <bokupediadestek@gmail.com>
2025-01-30 13:01:30 +0300

    Admin.py

--- discussion_forum/admin.py
+++ discussion_forum/admin.py
@@ -3,9 +3,16 @@ from .models import Category, Topic, Post
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
+    list_display = ('id', 'name', 'short_description') 
     search_fields = ('name',)  
-    ordering = ('name',) 
+    ordering = ('name',)
+
+    def short_description(self, obj):
+        """Description alanının ilk 10 kelimesini döndür"""
+        return ' '.join(obj.description.split()[:10]) + ('...' if len(obj.description.split()) > 10 else '')
+
+    short_description.short_description = "Description (Fırst 10 Words)" 
+
 
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
@@ -16,7 +23,7 @@ class TopicAdmin(admin.ModelAdmin):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'topic', 'author', 'created_at')
-    list_filter = ('topic', 'author') 
-    search_fields = ('content', 'author__username', 'topic__title')  
+    list_display = ('id', 'topic', 'category', 'author', 'created_at')
+    list_filter = ('topic', 'category', 'author')  
+    search_fields = ('content', 'author__username', 'topic__title', 'category__name') 
     ordering = ('-created_at',)
--- post_interactions/admin.py
+++ post_interactions/admin.py
@@ -1,9 +1,30 @@
 from django.contrib import admin
-from .models import *
+from .models import Haha, Dislike, Angry, Poop, Nauseated
 
-admin.site.register(Haha)
-admin.site.register(Dislike)
-admin.site.register(Comment)
-admin.site.register(Angry)
-admin.site.register(Poop)
-admin.site.register(Nauseated)
+class BaseReactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_post_id', 'post')
+
+    def get_post_id(self, obj):
+        return obj.post.id if obj.post else None
+    get_post_id.short_description = 'Post ID'
+
+class HahaAdmin(BaseReactionAdmin):
+    pass
+
+class DislikeAdmin(BaseReactionAdmin):
+    pass
+
+class AngryAdmin(BaseReactionAdmin):
+    pass
+
+class PoopAdmin(BaseReactionAdmin):
+    pass
+
+class NauseatedAdmin(BaseReactionAdmin):
+    pass
+
+admin.site.register(Haha, HahaAdmin)
+admin.site.register(Dislike, DislikeAdmin)
+admin.site.register(Angry, AngryAdmin)
+admin.site.register(Poop, PoopAdmin)
+admin.site.register(Nauseated, NauseatedAdmin)
