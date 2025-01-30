a7b102ccc875fda84e6efb6e1cd8e0587a4936eb
DogukanTaskiran <bokupediadestek@gmail.com>
2025-01-30 13:58:36 +0300

    Comment Admin

--- post_interactions/admin.py
+++ post_interactions/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Haha, Dislike, Angry, Poop, Nauseated
+from .models import Haha, Dislike, Angry, Poop, Nauseated, Comment
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'get_post_id', 'content', 'created_at', 'get_topic_title')
+    search_fields = ('author__username', 'content')
+    list_filter = ('created_at', 'author')
+
+    def get_post_id(self, obj):
+        return obj.post.id if obj.post else None
+    get_post_id.short_description = 'Post ID'
+
+    def get_topic_title(self, obj):
+        return obj.post.topic.title if obj.post.topic else "No Topic"
+    get_topic_title.short_description = 'Topic Title'
+
+admin.site.register(Comment, CommentAdmin)
 
 class BaseReactionAdmin(admin.ModelAdmin):
     list_display = ('user', 'get_post_id', 'post')
