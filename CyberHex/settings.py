32d9308529c62d21e2066ac4fcafa5f6bf2294b4
DogukanTaskiran <bokupediadestek@gmail.com>
2025-02-14 14:00:51 +0300

    Staticfiles storage

--- CyberHex/settings.py
+++ CyberHex/settings.py
@@ -152,6 +152,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
